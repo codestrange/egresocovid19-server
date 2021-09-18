12ef993ec4853d682e3d8d87492eba093b3d9dfb
leynier <leynier41@gmail.com>
2021-09-18 14:35:56 -0400

    feat: move code for initializing provinces data to another file
    This movement is for implement a CLI command later that makes the same.

--- egresocovid19/main.py
+++ egresocovid19/main.py
@@ -1,13 +1,10 @@
-from typing import List
-
 from beanie import init_beanie
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .api.v1.main import create_api as create_api_v1
-from .database import MunicipalityEmbeddedEntity, ProvinceEntity, client, entities
-from .static.municipality_codes import municipality_codes
-from .static.provinces_codes import province_codes
+from .database import client, entities
+from .seedbed import initialize_provinces_data
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
@@ -29,25 +26,7 @@ async def startup():
         database=client.egresocovid19,
         document_models=entities,  # type: ignore
     )
-
-    if not await ProvinceEntity.find_many().count():
-        provinces: List[ProvinceEntity] = []
-        for province_code, province_name in province_codes.items():
-            municipalities: List[MunicipalityEmbeddedEntity] = []
-            for municipality_code, municipality_name in municipality_codes.items():
-                if municipality_code.startswith(province_code):
-                    municipalities.append(
-                        MunicipalityEmbeddedEntity(
-                            name=municipality_name,
-                        )
-                    )
-            provinces.append(
-                ProvinceEntity(
-                    name=province_name,
-                    municipalities=municipalities,
-                )
-            )
-        await ProvinceEntity.insert_many(provinces)
+    await initialize_provinces_data()
 
 
 @app.on_event("shutdown")
