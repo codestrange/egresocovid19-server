dbcbbf3023f4c488b597330b97300b08f9738ea1
RodroVMS <democlesvanshepars@gmail.com>
2021-09-17 20:53:24 -0400

    feat: fill provinces on startup if none exists

--- egresocovid19/main.py
+++ egresocovid19/main.py
@@ -1,9 +1,13 @@
 from beanie import init_beanie
+from beanie.odm.fields import PydanticObjectId
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .api.v1.main import create_api as create_api_v1
-from .database import client, entities
+from .database import MunicipalityEmbeddedEntity, ProvinceEntity, client, entities
+
+from .static.provinces_codes import province_codes
+from .static.municipality_codes import municipality_codes
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
@@ -25,7 +29,21 @@ async def startup():
         database=client.egresocovid19,
         document_models=entities,  # type: ignore
     )
-
+    if len(await ProvinceEntity.find_all().to_list()) == 0:
+        for province_code in province_codes:
+            municipalities = []
+            for municip_code in municipality_codes:
+                if municip_code[:2] == province_code:
+                    municipalities.append(MunicipalityEmbeddedEntity(
+                        id = PydanticObjectId(),
+                        name = municipality_codes[municip_code]
+                    ))
+            await ProvinceEntity.insert_one(ProvinceEntity(
+                    name = province_codes[province_code],
+                    municipalities = municipalities
+                )
+            ) 
+        
 
 @app.on_event("shutdown")
 async def shutdown():
