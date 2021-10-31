c9817adc964bf0c7db9e49c085087dffae9d3e8f
leynier <leynier41@gmail.com>
2021-10-31 09:39:21 -0400

    fix: use python 3.8

--- egresocovid19/api/v1/controllers/__init__.py
+++ egresocovid19/api/v1/controllers/__init__.py
@@ -3,8 +3,9 @@ from os import listdir
 from os.path import abspath, dirname, isfile, join
 
 base_path = abspath(dirname(__file__))
+suffix = ".py"
 controllers = [
-    f.removesuffix(".py")
+    f[: -len(suffix)] if f.endswith(suffix) else f
     for f in listdir(base_path)
     if f != "__init__.py" and isfile(join(base_path, f))
 ]
